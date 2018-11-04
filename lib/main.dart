5c4eb5f7a96bc6ca5efa0eaf1186d4f165bf92d3
sylvain <sylvain@stilde.fr>
2018-11-04 15:01:10 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
 import 'screens/note_list.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(new MyApp());
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+   return new SplashScreen(
+      seconds: 3,
+      navigateAfterSeconds: new AfterSplash(),
+      title: new Text('CARNET DE NOTES',
+      style: new TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20.0
+      ),),
+      image: new Image.asset('assets/img/splash.png'),
+    
+      backgroundColor: Colors.white,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 100.0,
+      onClick: ()=>print("Stilde"),
+      loaderColor: Colors.red
+    );
+    
+  }
+}
+ 
+    
+    class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Carnet de notes',
